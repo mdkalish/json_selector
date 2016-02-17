a10b0980399bc248eb4f444593610726e3cda7cb
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-02-17 13:44:16 +0100

    dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-export function render(domElement){
+export function render(domElement, fullcontactData, sObjectData){
+  console.log('fullcontactData')
+  console.log(fullcontactData)
+  console.log('sObjectData')
+  console.log(sObjectData)
+
   ReactDOM.render(<App />, domElement)
 };

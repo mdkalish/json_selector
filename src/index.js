137428977c69fc1d4aa954a2b3f8ee74219efbdd
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2016-02-17 21:41:51 +0100

    Improve for local development

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const fullcontactKeys = ["status", "requestId", "likelihood", "photos", "contactInfo", "demographics", "socialProfiles"]
+
+// ONLY FOR LOCAL DEVELOPMENT
 const fullcontactData = require('json!./../narzedzia/fullcontact_ws.json')
 const sObjectData = require('json!./../narzedzia/fullcontact_mk.json')
 
-export function render(domElement) { //, fullcontactData, sObjectData) {
+window.fullcontactData = fullcontactData;
+window.sObjectData = sObjectData;
+/// ONLY FOR LOCAL DEVELOPMENT
+
+export function render(domElement, fullcontactData, sObjectData) {
   /*
   console.log('fullcontactData')
   console.log(fullcontactData)
